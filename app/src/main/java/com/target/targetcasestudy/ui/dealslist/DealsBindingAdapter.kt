3629da2ecdcff739db823f800853cebdfd3c3e0f
Prabhat <singh.prabhat8991@gmail.com>
2021-06-15 22:41:50 +0530

    webview added and layout refactored

--- app/src/main/java/com/target/targetcasestudy/ui/dealslist/DealsBindingAdapter.kt
+++ app/src/main/java/com/target/targetcasestudy/ui/dealslist/DealsBindingAdapter.kt
@@ -1,5 +1,6 @@
 package com.target.targetcasestudy.ui.dealslist
 
+import android.webkit.WebView
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
@@ -7,4 +8,9 @@ import com.bumptech.glide.Glide
 @BindingAdapter("imageUrl")
 fun ImageView.setImageUrl(url: String) {
     Glide.with(this.context).load(url).into(this)
+}
+
+@BindingAdapter("webContent")
+fun WebView.setContent(content: String) {
+    loadData(content, "text/html", "utf-8")
 }
